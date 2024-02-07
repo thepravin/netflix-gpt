9456afadd5eb0665d4f558307c448ef5a6d2c1b2
Pravin Nalawade <pravinonline05@gmail.com>
2024-02-07 18:06:52 +0530

    api added

--- src/utils/constatnts.js
+++ src/utils/constatnts.js
@@ -3,3 +3,17 @@ export const LOGO =
 
 export const BAGROUND =
   "https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+
+export const API_KEY = "21d347a1fc7d907b0621809f0138f83e";
+
+export const API_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWQzNDdhMWZjN2Q5MDdiMDYyMTgwOWYwMTM4ZjgzZSIsInN1YiI6IjY1YzM3MmY3OGUyZTAwMDE4M2E2NGE5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0-ncV5EzPqwlQnQHpupvsR_2o10K9SK01TZiS2rA1T4";
+
+
+  export const API_OPTIONS =  {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWQzNDdhMWZjN2Q5MDdiMDYyMTgwOWYwMTM4ZjgzZSIsInN1YiI6IjY1YzM3MmY3OGUyZTAwMDE4M2E2NGE5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0-ncV5EzPqwlQnQHpupvsR_2o10K9SK01TZiS2rA1T4'
+    }
+  };
