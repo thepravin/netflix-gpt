f9dd9c1d32a4479688d2c9f536a0a00ae1c9b3ee
Pravin Nalawade <pravinonline05@gmail.com>
2024-02-07 18:40:42 +0530

    add movieslice

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constatnts'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovies } from '../utils/movieSlice'
 
 const Browse = () => {
-
+const dispatch = useDispatch();
 
 
   /* API CALL */
   const getNowPlaying = async ()=>{
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
     const json = await data.json();
-    console.log(json.results)
+    //console.log(json.results)
+    dispatch(addNowPlayingMovies(json.results))
   }
   useEffect(()=>{
     getNowPlaying();
