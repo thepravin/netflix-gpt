a8988db75c9695ab0da76f37cf0bfbc9c0ff5402
Pravin Nalawade <pravinonline05@gmail.com>
2024-02-09 14:33:06 +0530

    background video added

--- src/Components/SecondaryContainer.js
+++ src/Components/SecondaryContainer.js
@@ -3,7 +3,9 @@ import React from 'react'
 
 const SecondaryContainer = () => {
   return (
-    <div>SecondaryContainer</div>
+    <div>
+      
+    </div>
   )
 }
 
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className='pt-36 px-12'>
+    <div className='w-screen aspect-video  pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-6xl font-bold'>{title}</h1>
         <p className='py-6 w-1/3 text-lg'>{overview}</p>
         <div >
-            <button className='p-2 px-10 text-lg mr-5 bg-red-600 text-black rounded-lg  '>▶️Play</button>
-            <button className='p-2 px-8 text-lg mr-5 bg-gray-400 text-black rounded-lg  '>More Info</button>
+            <button className='p-2 px-10 text-lg mr-5 bg-red-600 font-bold text-black rounded-lg hover:opacity-80 '>▶️Play</button>
+            <button className='p-2 px-8 text-lg mr-5 font-bold bg-gray-400 text-black rounded-lg  hover:opacity-80 '>More Info</button>
         </div>
 
     </div>
